5045fc9bdfb6620af1495f21c18b93dec2ae7334
“Matthew <mherndon@ou.edu>
2020-04-13 20:05:15 -0500

    - using maximum likelihood estimation to design boxcox transform where data isn't sufficiently normal - now includes qq critical point table for determining normality

--- R/normal.R
+++ R/normal.R
@@ -1,12 +1,34 @@
 library(future)
 library(rprojroot)
+library(pracma)
 
 root <- rprojroot::is_r_package
-qq_critical_points <- future(
-  read.csv(root$find_file("table_4_2_qq_critical.csv"))
-)
+# Table 4.2 from the book
+qq_critical_points = read.csv(root$find_file("table_4_2_qq_critical.csv"))
 
-#readLines(root$find_file("table_4_2_qq_critical.csv"), 3)
+
+boxcox = function(x,lambda){
+  x = matrix(x)
+  lambda = matrix(lambda,byrow=TRUE)
+  print(dim(x))
+  print(dim(lambda))
+  # use box cox transformation
+  x_l = lambda
+  x_l[lambda==0] = log(x[lambda==0])
+  x_l[lambda!=0] = (x[lambda!=0]^(lambda[lambda!=0])-1)/lambda[lambda!=0]
+  return(x_l)
+}
+
+box_cox_maximization = function(x,lambda){
+  # box-cox likelihood function, target for maximization
+  n = length(x)
+  m = length(lambda)
+  xs = sum(log(x))
+  x = repmat(matrix(x,nrow=n,nc=1),1,m)
+  x_to_the_lambda = boxcox(x,repmat(matrix(lambda,nrow=1,nc=m),n,1))
+  l = -n/2 * log(1/n * colSums((x_to_the_lambda-mean(x_to_the_lambda))^2)) + (lambda-1)*xs
+  return(l)
+}
 
 assess_normality = function(x, significance=5){
   # number of samples in variable
@@ -43,7 +65,14 @@ normal = function(dataset)
   
   tn = assess_normality(dset_numeric[,1])
   
-  obj = list(original=dataset, transformed_normal=tn)
+  opt_interval = c(-5,5)
+  lambda_opt = optimize(
+    function(lambda) box_cox_maximization(iris[,1],lambda), 
+    c(-5,5),
+  )
+  print(lambda_opt)
+  
+  obj = list(original=dataset, transformed_normal=tn, lambda=lambda)
   # obj is now a list of class `list` and `normal`
   class(obj) = append(class(obj),"normal") 
   return(obj)
